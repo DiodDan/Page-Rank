16e49ac9313df755977017cfa80bdd515b077841
DIOD <59664412+danila12315454@users.noreply.github.com>
2024-07-07 17:35:58 +0200

    Data Representation added (#6)

--- python/Data_check.py
+++ python/Data_check.py
@@ -1,9 +1,13 @@
 import os
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
+from pathlib import Path
+from tqdm import tqdm
+
 
 def get_html_files(directory):
     return list({f for f in os.listdir(directory) if f.endswith('.html')})
+
 
 def extract_links(html_content, base_url):
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -21,31 +25,35 @@ def extract_links(html_content, base_url):
 
     return links
 
+
 def create_links_list(directory):
     html_files = get_html_files(directory)
     links_list = []
 
-    for html_file in html_files:
+    for html_file in tqdm(html_files):
         file_path = os.path.join(directory, html_file)
         with open(file_path, 'r', encoding='utf-8') as file:
             html_content = file.read()
 
-        base_url = f'file://{os.path.abspath(file_path)}'
-        links = extract_links(html_content, base_url)
+        links = extract_links(html_content, os.path.abspath(file_path))
         for link in links:
-            file = link[13:]+".html"
-            if  ('/wiki/') in link and file in html_files:
-                links_list.append((html_file, link[13:]+".html"))
+            file = link[6:] + ".html"
+            if file in html_files:
+                links_list.append((html_file, file))
 
     return links_list
 
+
 def main():
-    directory = 'wikipedia_pages'
+    directory = Path(__file__).parent.parent / 'wikipedia_pages'
     links_list = create_links_list(directory)
 
-    for link in links_list:
-        print(link)
+    information = "\n".join([f'{source} -> {target}' for source, target in links_list])
+    print(information)
+
+    Path('links.txt').write_text(information)
+
+
 
 if __name__ == '__main__':
     main()
-
